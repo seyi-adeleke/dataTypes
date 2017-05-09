12ea28dc2978522ba1f5bbb7f201505c686116cc
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-05-09 12:12:07 +0100

    implemented es6 syntax, code cleanup, added comments

--- dataTypes.js
+++ dataTypes.js
@@ -1,11 +1,13 @@
-exports.dataTypes = function(argument){
-   if(Array.isArray(argument)){
-       if (argument.length ===0){
+exports.dataTypes =(argument) => {
+    // check if argument is an array
+    if(Array.isArray(argument)){
+       if (argument.length === 0){
            return undefined;
        }
-   }
+    }
+    // check data type and return appropriate value
     if(typeof argument === "undefined" || argument === null){
-        return 'no value'
+        return 'no value';
     }
     if(typeof argument === "boolean"){
         return argument;
@@ -13,6 +15,8 @@ exports.dataTypes = function(argument){
     if(typeof argument === "string"){
         return argument.length;
     }
+
+    // compare argument with 100
     if(argument === 100){
         return "equal to 100";
     }
@@ -25,15 +29,15 @@ exports.dataTypes = function(argument){
 
 
     if(Array.isArray(argument)){
-
         if(argument[2]=== null){
             return undefined;
         }
-
-       else{
-        return argument[2];
+        else{
+            return argument[2];
         }
     }
+
+    // check if argument is a function
     if(typeof argument === "function"){
         return argument(true);
     }
