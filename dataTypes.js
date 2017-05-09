7990cf107e8bc0c9d455d6762c33e338ede34128
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-05-09 10:55:24 +0100

    passed tests for comparing with 100 and length of string

--- dataTypes.js
+++ dataTypes.js
@@ -1,8 +1,21 @@
 exports.dataTypes = function(argument){
-    if(typeof argument === "undefined" || typeof argument === "undefined"){
+    if(typeof argument === "undefined" || typeof argument === "object"){
         return 'no value'
     }
     if(typeof argument === "boolean"){
         return argument;
     }
+    if(typeof argument === "string"){
+        return argument.length;
+    }
+    if(argument === 100){
+        return "equal to 100";
+    }
+    if(argument < 100){
+        return "less than 100";
+    }
+    if(argument > 100){
+        return "more than 100";
+    }
+
 };
