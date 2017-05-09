6db5f6af2234e6599ad6eb9fc08f838376a2948c
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-05-09 10:45:40 +0100

    passed test for boolean values

--- dataTypes.js
+++ dataTypes.js
@@ -2,4 +2,7 @@ exports.dataTypes = function(argument){
     if(typeof argument === "undefined" || typeof argument === "undefined"){
         return 'no value'
     }
+    if(typeof argument === "boolean"){
+        return argument;
+    }
 };
